db90f4bd97bc10fc1c6f536607eb095ea74e26d1
Julian Smith <julian.smith@artifex.com>
2024-12-09 18:13:34 +0000

    tests/: added test_4125(), for pixmap memory leak.
    Addresses #4125.

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -115,3 +115,98 @@ def test_4090():
         assert 0.93 <= r < 1.05, f'{r1=} {r2=} {r=}.'
     else:
         assert 0.95 <= r < 1.05, f'{r1=} {r2=} {r=}.'
+
+
+def show_tracemalloc_diff(snapshot1, snapshot2):
+    top_stats = snapshot2.compare_to(snapshot1, 'lineno')
+    n = 0
+    mem = 0
+    for i in top_stats:
+        n += i.count
+        mem += i.size
+    print(f'{n=}')
+    print(f'{mem=}')
+    print("Top 10:")
+    for stat in top_stats[:10]:
+        print(f'    {stat}')
+    snapshot_diff = snapshot2.compare_to(snapshot1, key_type='lineno')
+    print(f'snapshot_diff:')
+    count_diff = 0
+    size_diff = 0
+    for i, s in enumerate(snapshot_diff):
+        print(f'    {i}: {s.count=} {s.count_diff=} {s.size=} {s.size_diff=} {s.traceback=}')
+        count_diff += s.count_diff
+        size_diff += s.size_diff
+    print(f'{count_diff=} {size_diff=}')
+    
+
+
+def test_4125():
+    if os.environ.get('PYMUPDF_RUNNING_ON_VALGRIND') == '1':
+        print(f'test_4125(): not running because PYMUPDF_RUNNING_ON_VALGRIND=1.')
+        return
+    if platform.system().startswith('MSYS_NT-'):
+        print(f'test_4125(): not running on msys2 - psutil not available.')
+        return
+    
+    print(f'test_4125():')
+    
+    path = os.path.normpath(f'{__file__}/../../tests/resources/test_4125.pdf')
+    import gc
+    import psutil
+    
+    root = os.path.normpath(f'{__file__}/../..')
+    sys.path.insert(0, root)
+    try:
+        import pipcl
+    finally:
+        del sys.path[0]
+    
+    process = psutil.Process()
+    
+    class State: pass
+    state = State()
+    state.rsss = list()
+    state.prev = None
+    
+    def get_stat():
+        rss = process.memory_info().rss
+        if not state.rsss:
+            state.prev = rss
+        state.rsss.append(rss)
+        drss = rss - state.prev
+        state.prev = rss
+        print(f'test_4125():'
+                f' rss={pipcl.number_sep(rss)}'
+                f' rss-rss0={pipcl.number_sep(rss-state.rsss[0])}'
+                f' drss={pipcl.number_sep(drss)}'
+                f'.'
+                )
+    
+    for i in range(10):
+        with pymupdf.open(path) as document:
+            for page in document:
+                for image_info in page.get_images(full=True):
+                    xref, smask, width, height, bpc, colorspace, alt_colorspace, name, filter_, referencer = image_info
+                    pixmap = pymupdf.Pixmap(document, xref)
+                    if pixmap.colorspace != pymupdf.csRGB:
+                        pixmap2 = pymupdf.Pixmap(pymupdf.csRGB, pixmap)
+                        del pixmap2
+                    del pixmap
+        pymupdf.TOOLS.store_shrink(100)
+        pymupdf.TOOLS.glyph_cache_empty()
+        gc.collect()
+        get_stat()
+    
+    # Check memory leak is as expected.
+    for i in range(3, len(state.rsss)):
+        drss = state.rsss[i] - state.rsss[i-1]
+        if pymupdf.mupdf_version_tuple < (1, 25, 2):
+            drss_expected = 4915200
+            e = abs(1 - drss / drss_expected)
+            print(f'test_4125(): {i=} {e=}')
+            assert e < 0.15, f'{i=}: {drss_expected} {drss} {e=}.'
+        else:
+            # Bug fixed in MuPDF-1.26 and hopefully MuPDF-1.25.2.
+            e = 4096
+            assert abs(drss) <= e, f'{i=}: {drss=} {e=}.'
